432afb098be224e591f3fd4b55953b571b6632e5
Hugo Ruivo <717754+HRuivo@users.noreply.github.com>
2024-09-15 17:40:32 +0100

    custom build system with run step that starts the kernel with Qemu

---
+++ build.zig
@@ -0,0 +1,38 @@
+const std = @import("std");
+
+pub fn build(b: *std.Build) void {
+    const kernel = buildKernel(b);
+
+    const run_step = b.step("run", "Run in Qemu");
+    const run_qemu = b.addSystemCommand(&(.{
+        "qemu-system-x86_64",
+        "-kernel",
+        b.pathJoin(&.{ "zig-out/bin", kernel }),
+    }));
+    run_step.dependOn(&run_qemu.step);
+}
+
+fn buildKernel(b: *std.Build) []const u8 {
+    const target = b.resolveTargetQuery(.{
+        .cpu_arch = .x86,
+        .os_tag = .freestanding,
+        .abi = .none,
+        .cpu_model = std.Target.Query.CpuModel{ .explicit = &std.Target.x86.cpu.i386 },
+    });
+
+    const kernel = b.addExecutable(.{
+        .name = "kernel",
+        .root_source_file = b.path("src/kernel.zig"),
+        .target = target,
+        .optimize = b.standardOptimizeOption(.{}),
+        .strip = true,
+    });
+    kernel.setLinkerScript(b.path("src/kernel.ld"));
+
+    const copy_kernel_object = b.addInstallBinFile(kernel.getEmittedBin(), "kernel.o");
+
+    b.default_step.dependOn(&copy_kernel_object.step);
+    copy_kernel_object.step.dependOn(&kernel.step);
+
+    return copy_kernel_object.dest_rel_path;
+}
